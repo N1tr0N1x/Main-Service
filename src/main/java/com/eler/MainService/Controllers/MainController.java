821a819b0e7d8eca63f0a5ce43ce5a68b6457b67
ramiala <61067820+ramiala@users.noreply.github.com>
2021-03-23 12:40:32 +0100

    Update MainController.java
    save new module + return list of modules

--- src/main/java/com/eler/MainService/Controllers/MainController.java
+++ src/main/java/com/eler/MainService/Controllers/MainController.java
@@ -55,5 +55,36 @@ public class MainController {
     public UserAccount login(@ModelAttribute("UserAccount") UserAccount user) {
         return restTemplate.getForObject("http://Authentication-Service/auth/login/" + user.getIdUser() + "/" + user.getPassword(),UserAccount.class);
     }
+     @PostMapping("/save")
+    public String save(@ModelAttribute("Module") Module module) {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.postForObject("http://localhost:8085/mod/save/",module, String.class);
+        
+    }
+
+
+    @JsonIgnoreProperties
+    @RequestMapping(value = "/modindex", method = RequestMethod.POST,headers = "Accept=application/json")
+    @ResponseBody
+    //public String viewHomePage(Model model, @RequestBody List<Module> modules) {
+    public ModelAndView viewIndexModule(Model model,@RequestBody String modules) {
+        ModelAndView modelAndView = new ModelAndView();
+        final ObjectMapper objectMapper = new ObjectMapper();
+        //objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        List<Module> mod = new ArrayList<Module>();
+        try{
+             mod = objectMapper.readValue(modules, new TypeReference<List<Module>>(){});
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //List<Module> mod = modules;
+        modelAndView.setViewName("moduleindex");
+        modelAndView.addObject("listmodules", mod);
+        return modelAndView;
+        
+    }   
 
 }
